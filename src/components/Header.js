8fc07067127561c84b6f9288e0d2b2b164de7752
SherpaMilan <msherpa574@icloud.com>
2023-02-22 19:29:43 +1100

    creating money tracker app

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="d-flex mt-3 justify-content-center">
+      Your Money Tracker App
+    </div>
+  );
+};
+
+export default Header;
